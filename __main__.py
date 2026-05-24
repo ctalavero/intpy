6b7689da94f0999ba2c5b2fe8484b7c1baf30a06
mykola fylypchuk <ctalavero12@gmail.com>
2026-05-24 15:43:48 +0300

    feat: implement application entrypoint and run scripts

---
+++ __main__.py
@@ -0,0 +1,4 @@
+from intpy.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from intpy.presentation.cli import run_cli
+
+
+def main() -> None:
+    sys.exit(run_cli())
+
+
+if __name__ == "__main__":
+    main()
